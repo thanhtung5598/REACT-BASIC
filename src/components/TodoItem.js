d2b6179fa9f3dd7a57de527f15430571838be7bf
LeThanhTung <thanhtung5598@gmail.com>
2019-12-31 20:21:16 +0700

    add folder components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div className="TodoItem">
+                <p>New compoenent</p>
+            </div>
+        )
+    }
+}
