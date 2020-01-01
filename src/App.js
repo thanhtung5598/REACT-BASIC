512783d5df142685cf4f02dc71baed049964c53b
LeThanhTung <thanhtung5598@gmail.com>
2020-01-01 18:53:13 +0700

    add new Items

--- src/App.js
+++ src/App.js
@@ -1,43 +1,93 @@
 import React, { Component } from 'react';
 // import TrafficLight from './components/traffic/TrafficLight';
 import TodoItems from './components/TodoItems/TodoItem';
+import tickIcon from './components/TodoItems/img/tick.png'
 import './App.css'
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
+      newItems: "",
       items: [
         { title: "Đá bóng", isComplete: true },
-        { title: "Nhảy dây",isComplete: false },
+        { title: "Nhảy dây" },
         { title: "Bắn bi" }
       ]
     }
-   
+
   }
 
   onItemClicked = (item) => {
     return (event) => {
-        const isComplete = item.isComplete;
-        const {items} = this.state;
-        const index = items.indexOf(item);
-        this.setState({
-          items:[
-            ...items.slice(0,index),
-            {
-              ...item,
-              isComplete:!isComplete
-            },
-            ...items.slice(index+1)
-          ]
-        })
+      const isComplete = item.isComplete;
+      const { items } = this.state;
+      const index = items.indexOf(item);
+      this.setState({
+        items: [
+          ...items.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...items.slice(index + 1)
+        ]
+      })
     }
   }
 
+  onKeyUp = (event) => {
+    let text = event.target.value;
+    if (!text) return
+    if (event.keyCode === 13) {
+      this.setState({
+        newItems: '',
+        items: [
+          { title: text, isComplete: false },
+          ...this.state.items
+        ]
+      })
+    }
+  }
+
+  onChangeValue = (event) => {
+    let { value } = event.target;
+    this.setState({
+      newItems: value
+    })
+  }
+
+  onClickAllItems = () => {
+    let allItems = [...this.state.items];
+    allItems.forEach((item)=>{
+      item.isComplete=true
+    })
+    this.setState({
+      newItems: '',
+      items: [
+        ...allItems
+      ]
+    })
+  }
+
   render() {
-    const { items } = this.state
+    const { items, newItems } = this.state
     return (
       <div className="TodoItems">
+        <div className="Header">
+          <img
+            src={tickIcon}
+            width={35} alt="Unload"
+            onClick={this.onClickAllItems}
+          />
+          <input
+            type="text"
+            placeholder="Add New Item"
+            onKeyUp={this.onKeyUp}
+            value={newItems}
+            onChange={this.onChangeValue}
+          />
+        </div>
         {
           items.length > 0 && items.map((item, index) => {
             return (
--- src/components/TodoItems/TodoItem.js
+++ src/components/TodoItems/TodoItem.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './TodoItem.css'
+import check from './img/check.png';
+import checked from './img/checked.png';
 
 export default class TodoItem extends Component {
     render() {
-        const { item,onClick } = this.props;
+        const { item, onClick } = this.props;
         return (
-            <div onClick={onClick} className={item.isComplete===true ? "TodoItem TodoItem-isComplete" : "TodoItem"}>
+            <div className={item.isComplete === true ? "TodoItem TodoItem-isComplete" : "TodoItem"}>
+                <img onClick={onClick} width={40} src={item.isComplete === true ? checked : check} alt="unloaded" />
                 <p>{item.title}</p>
             </div>
         )
